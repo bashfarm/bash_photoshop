e07cdf2c381f5d7a66088fdc9ad63f1520916c5a
Benjamin Garrard <b2gdevs@gmail.com>
2022-11-22 14:01:59 -0600

    merge webpack

--- src/panels/UxpStorage.jsx
+++ src/panels/UxpStorage.jsx
@@ -1,18 +1,27 @@
-import React, { useState, useEffect } from 'react';
-import {
-    PlaceImageFromDataOnLayer,
-    MergeAndSaveAllVisibleLayersIntoImage,
-    IsMoreThanOneVisibleLayer,
-} from 'utils/layer_service';
-import {
-    GetDataFolderImageBase64ImgStr,
-    SaveB64ImageToBinaryFileToDataFolder,
-} from 'utils/io_service';
-import { Img2Img, FormatBase64Image } from 'utils/ai_service';
-import { alert } from 'utils/general_utils';
+const fs = require('uxp').storage.localFileSystem;
+const photoshop = require('photoshop');
+const app = photoshop.app;
+const bp = photoshop.action.batchPlay;
+const executeAsModal = photoshop.core.executeAsModal;
 import '../style.css';
+import { MergeAndSaveAllVisibleLayersIntoImage, IsMoreThanOneVisibleLayer, PlaceImageFromDataOnLayer, SetNewestLayerOnTop } from '../utils/layer_service';
+import {
+    SaveMergedLayersImgPNGToDataFolder,
+    GetDataFolderImageBase64ImgStr,
+    SaveTextFileToDataFolder,
+    SaveB64ImageToBinaryFileToDataFolder
+} from '../utils/io_service';
+import { useState, useEffect } from 'react';
+import { Img2Img } from '../utils/ai_service';
+import { alert } from '../utils/general_utils';
+import { FormatBase64Image } from '../utils/ai_service';
 
+function IsBase64(b64str){
+    return b64str.includes("=")
+}
 export const UxpStorage = () => {
+    
+
     var [base64MergedImgStr, SetBase64MergedImgStr] = useState('m');
     var [base64GeneratedImgStr, SetBase64GeneratedImgStr] = useState('m');
 
@@ -21,58 +30,80 @@ export const UxpStorage = () => {
             SetBase64MergedImgStr(b64str['imageHeader'] + b64str['base64Data']);
         });
 
-    const GenerateImage = async () => {
-        var generatedImageResponse = await Img2Img(base64MergedImgStr);
-        // Set the first generated image to the generated image string
-        SetBase64GeneratedImgStr(
-            FormatBase64Image(generatedImageResponse['images'][0])
-        );
-    };
 
-    useEffect(() => {
-        SaveB64ImageToBinaryFileToDataFolder(
-            'generatedFile.png',
-            base64GeneratedImgStr
-        );
-    }, [base64GeneratedImgStr]);
+    const GenerateImage = async () => {
+        try{
+            if(!IsBase64(base64GeneratedImgStr))
+            {
+                console.log(`We are trying to generate an image in the AI using this b64 string => '${base64GeneratedImgStr}.'🤦‍♂️`)
+                console.log(`Also generated file is not in the correct base64 format '${base64GeneratedImgStr}'🙄`)
+            }
+            var generatedImageResponse = await Img2Img(base64MergedImgStr);
+            console.log("🔥🔥 Generated image form app.js 🔥🔥")
+        } catch(e){
+            console.log(e)
+        }
+            // Set the first generated image to the generated image string
+            SetBase64GeneratedImgStr(FormatBase64Image(generatedImageResponse["images"][0]))
+
+
+    
+    }
+
+    useEffect(()=> {
+        if (!IsBase64(base64GeneratedImgStr))
+        {
+            console.log(`we are trying to set the generated image b64 string in the beginning component lifecycle when it is just '${base64GeneratedImgStr}'🙄`)
+            console.log(`Also generated file is not in the correct base64 format cause it is '${base64GeneratedImgStr}'🙄`)
+
+        }
+        else
+            SaveB64ImageToBinaryFileToDataFolder("generatedFile.png", base64GeneratedImgStr)
+
+    }, [base64GeneratedImgStr])
 
     return (
-        <div className="column">
-            <sp-action-button
-                onClick={async () => {
-                    try {
-                        // SaveTextFileToDataFolder("yolo.txt", "yolo")
-                        if (IsMoreThanOneVisibleLayer()) {
-                            MergeAndSaveAllVisibleLayersIntoImage();
-                            SetMergedImageBase64();
-                            await GenerateImage();
-                            await SaveB64ImageToBinaryFileToDataFolder(
-                                'generatedImg.png',
-                                base64MergedImgStr
-                            );
+        <>
+            <div className="column">
+                <sp-action-button
+                    onClick={async () => {
+                        try {
+                            // SaveTextFileToDataFolder("yolo.txt", "yolo")
+                            if (IsMoreThanOneVisibleLayer()) {
+                                MergeAndSaveAllVisibleLayersIntoImage()
+                                var delayInMilliseconds = 2000; //1 second
 
-                            PlaceImageFromDataOnLayer('generatedFile.png');
-                        } else {
-                            alert('Not enough layers!');
-                            console.log('not enough layers');
+                                setTimeout(function() {
+                                //your code to be executed after 1 second
+                                }, delayInMilliseconds);
+                                SetMergedImageBase64()
+                                await SetNewestLayerOnTop()
+                                await GenerateImage()
+                                PlaceImageFromDataOnLayer("generatedFile.png")
+
+                            } else {
+                                alert("Not enough layers Selected 😅!")
+                                console.log('not enough layers');
+                            }
+                        } catch (e) {
+                            console.log(e)
+                            console.log('couldnt run visible merge');
                         }
-                    } catch (e) {
-                        console.log(e);
-                        console.log('couldnt run visible merge');
-                    }
-                }}
-            >
-                CombineLayers
-            </sp-action-button>
-            {/* <sp-action-button onClick={SetMergedImageBase64}>Get Base 64 </sp-action-button> */}
-            <sp-label>{JSON.stringify(base64MergedImgStr)}</sp-label>
-            {/* This below can work */}
-            {/* <sp-action-button onClick={GenerateImage}>Generate New Image</sp-action-button> */}
-            <div className="row" style={{ alignItems: 'stretch' }} />
-            {/* Just rendering the newly generated image */}
-            <div className="image-container">
-                <img src={base64GeneratedImgStr} alt="preview-image" />
+                    }}
+                >
+                    CombineLayers
+                </sp-action-button>
+                {/* <sp-action-button onClick={SetMergedImageBase64}>Get Base 64 </sp-action-button> */}
+                <sp-label>{JSON.stringify(base64MergedImgStr)}</sp-label>
+                {/* This below can work */}
+                {/* <sp-action-button onClick={GenerateImage}>Generate New Image</sp-action-button> */}
+                <div className="row" style={{ alignItems: "stretch" }}/>
+                {/* Just rendering the newly generated image */}
+                <div className="image-container">
+                    <img src={base64GeneratedImgStr} alt="preview-image" />
+                </div>
+
             </div>
-        </div>
+        </>
     );
 };
--- src/utils/layer_service.js
+++ src/utils/layer_service.js
@@ -3,8 +3,9 @@ const fs = require('uxp').storage.localFileSystem;
 const app = photoshop.app;
 const bp = photoshop.action.batchPlay;
 const executeAsModal = photoshop.core.executeAsModal;
-import { SaveMergedLayersImgPNGToDataFolder } from './io_service';
+const lfs = require('uxp').storage.localFileSystem;
 
+import { SaveMergedLayersImgPNGToDataFolder } from './io_service';
 /**
  * The entry point to the service.  For now...
  * @returns null if there is not more than one visible layer
@@ -18,11 +19,6 @@ export async function MergeAndSaveAllVisibleLayersIntoImage() {
             return;
         }
 
-        const res = await executeAsModal(async () => {
-            var visibleLayers = GetVisibleLayers();
-            console.log(visibleLayers);
-            visibleLayers[0].selected = true;
-        });
         await mergeVisibleLyrs();
         await MakeLayersInvisible();
         SaveMergedLayersImgPNGToDataFolder();
@@ -67,30 +63,37 @@ export function IsMoreThanOneVisibleLayer() {
 }
 
 async function mergeVisibleLyrs() {
-    const res = await executeAsModal(async () => {
-        await bp(
-            [
-                {
-                    _obj: 'mergeVisible',
-                    duplicate: true,
-                    _options: {
-                        dialogOptions: 'dontDisplay',
+    try{
+        const res = await executeAsModal(async () => {
+            var visibleLayers = GetVisibleLayers();
+            visibleLayers[0].selected = true;
+            await bp(
+                [
+                    {
+                        _obj: 'mergeVisible',
+                        duplicate: true,
+                        _options: {
+                            dialogOptions: 'dontDisplay',
+                        },
                     },
-                },
-            ],
-            { commandName: 'Generate Combined Layers' }
-        );
-    });
-    console.log('finished Merging Layers');
+                ],
+                { commandName: 'Generate Combined Layers' }
+            );
+        });
+        console.log('finished Merging Layers');
+    } catch(e){
+        console.log(e)
+    }
+
 }
 
 export const PlaceImageFromDataOnLayer = async (imageName) => {
     try {
-        const dataFolder = await fs.getDataFolder();
+        const dataFolder = await lfs.getDataFolder();
         var placedDocument = await dataFolder.getEntry(imageName);
         if (!placedDocument) return;
-        let tkn = fs.createSessionToken(placedDocument);
-        await executeAsModal(
+        let tkn = lfs.createSessionToken(placedDocument);
+        const res = await executeAsModal(
             async () => {
                 await bp(
                     [
@@ -110,3 +113,47 @@ export const PlaceImageFromDataOnLayer = async (imageName) => {
         console.log(e);
     }
 };
+
+
+/***
+ * Not working
+ */
+export async function SetNewestLayerOnTop(){
+    var layers = GetVisibleLayers();
+    var maxLayer = Math.max(...layers.map(o => o._id))
+    const res = await executeAsModal(
+        async () => {
+    removeItem(app.activeDocument.activeLayers, maxLayer)
+    app.activeDocument.activeLayers.push(maxLayer)
+        })
+    
+}
+
+function removeItem(arr, value) {
+    var index = arr.indexOf(value);
+    if (index > -1) {
+      arr.splice(index, 1);
+    }
+    return arr;
+  }
+
+async function SelectLayer(layer){
+    const res = await executeAsModal(
+        async () => {
+            await bp(
+                [
+                    {
+                      "_obj": "select",
+                      "_target": [{ "_name": layer._name, "_ref": "layer" }],
+                      "layerID": [layer._id],
+                      "makeVisible": true
+                    }
+                  ]
+                  ,
+                {}
+            );
+        },
+        { commandName: 'Select Layer' }
+    );
+      
+}
