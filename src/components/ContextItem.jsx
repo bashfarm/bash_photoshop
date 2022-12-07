8359005a2f3356856b31122dbe9fbbdeac695c3e
Benjamin Garrard <b2gdevs@gmail.com>
2022-12-07 12:33:35 -0600

    it is working again, but we aren't deleting the layer and keeping the same context.  IT works for a demo

--- src/components/ContextItem.jsx
+++ src/components/ContextItem.jsx
@@ -1,17 +1,12 @@
 import { useState, useEffect } from 'react';
-import {
-    Heading,
-    Textarea,
-} from 'react-uxp-spectrum';
+import { Heading, Textarea } from 'react-uxp-spectrum';
 import { Progressbar } from 'react-uxp-spectrum/dist';
 import { CreateAILayerContextId, useAppStore } from '../store/appStore';
 import {
     GetImageProcessingProgress,
-	RegenerateLayer,
+    RegenerateLayer,
 } from '../utils/ai_service';
-import {
-    GetHistoryFilePaths,
-} from '../utils/io_service';
+import { GetHistoryFilePaths } from '../utils/io_service';
 import { ProgressButton } from './ProgressButton';
 const fs = require('uxp').storage.localFileSystem;
 const photoshop = require('photoshop');
@@ -23,16 +18,21 @@ export const ContextItem = ({ layerContext = {} }) => {
     let setAILayerContext = useAppStore((state) => state.setAILayerContext);
     let layerAIContexts = useAppStore((state) => state.layerAIContexts);
     let replaceAILayerContext = useAppStore((state) => state.getAILayerContext);
-    let getAILayerContext = useAppStore((state) => state.replaceAILayerContext);
-    let setAILayerContextPrompt = useAppStore((state) => state.setAILayerContextPrompt);
-	
+    let setLayerid2ContextId = useAppStore(
+        (state) => state.setLayerid2ContextId
+    );
+    let setAILayerContextPrompt = useAppStore(
+        (state) => state.setAILayerContextPrompt
+    );
+    let removeLayerid2ContextId = useAppStore(
+        (state) => state.removeLayerid2ContextId
+    );
 
     useEffect(async () => {
         if (imageProgress == 1 || imageProgress == 0) {
             SetLayerContextHisoricalFiles(
                 await GetHistoryFilePaths(thisLayersContext)
             );
-
         }
     }, [imageProgress]);
 
@@ -42,12 +42,12 @@ export const ContextItem = ({ layerContext = {} }) => {
                 <div className="flex flex-col bg-brand-dark">
                     <Heading className="text-lg text-brand-dark">
                         <span className="text-lg text-white">
-                            {thisLayersContext.currentLayer.name}
+                            {thisLayersContext.layers[0].name}
                         </span>
                     </Heading>
                     <Heading className="text-lg text-brand-dark">
                         <span className="text-lg text-white">
-                            {thisLayersContext.currentLayer.id}
+                            {thisLayersContext.layers[0].id}
                         </span>
                     </Heading>
                 </div>
@@ -56,11 +56,18 @@ export const ContextItem = ({ layerContext = {} }) => {
                         // We have to have a standard image size for bashing process.  We can't allocate that much Vram for high resolutions
                         //  512x512 is the cheapest.  We will have to have a final step of upscaling
                         longRunningFunction={async () => {
-							console.log("regenerate Layer")
-							console.log(thisLayersContext)
-							console.log(layerAIContexts)
-							console.log(app.activeDocument.layers)
-
+                            console.log('regenerate Layer');
+                            console.log(thisLayersContext);
+                            console.log(layerAIContexts);
+                            console.log(app.activeDocument.layers);
+                            RegenerateLayer(
+                                512,
+                                512,
+                                thisLayersContext,
+                                setLayerid2ContextId,
+                                removeLayerid2ContextId,
+                                setAILayerContext
+                            );
                         }}
                         progressQueryFunction={GetImageProcessingProgress}
                         queryResponseParser={(response) => response['progress']}
@@ -79,12 +86,11 @@ export const ContextItem = ({ layerContext = {} }) => {
                 </div>
             </div>
             <div>
-			<Textarea
+                <Textarea
                     placeholder="Enter a description of the content in this layer"
                     onInput={(event) => {
-
-						// Making a copy like this and resetting seems to render things well
-						// using the app store methods causing a total rerender and makes this suck
+                        // Making a copy like this and resetting seems to render things well
+                        // using the app store methods causing a total rerender and makes this suck
                         let newContext = {
                             ...thisLayersContext,
                             currentPrompt: event.target.value,
@@ -93,7 +99,7 @@ export const ContextItem = ({ layerContext = {} }) => {
                             CreateAILayerContextId(thisLayersContext.layers[0]),
                             newContext
                         );
-						SetThisLayersContext(newContext)
+                        SetThisLayersContext(newContext);
                     }}
                     className="w-full"
                 ></Textarea>
