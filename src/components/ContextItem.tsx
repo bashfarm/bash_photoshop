8f2fa04c5e792bb9ddc7557669e15ced5da55144
Kevin Mok <mok.kevin11@gmail.com>
2022-12-18 21:35:06 -0600

    Add some styling to be theme agnostic - wip

--- src/components/ContextItem.tsx
+++ src/components/ContextItem.tsx
@@ -21,8 +21,8 @@ export const ContextItem = (props: ContextItemProps) => {
     );
 
     return (
-        <div className="flex flex-col bg-brand-dark">
-            <div className="flex flex-row bg-brand">
+        <div className="flex flex-col p-1 bg-[color:var(--uxp-host-widget-hover-background-color)] border border-[color:var(--uxp-host-border-color)] rounded">
+            <div className="flex">
                 <ContextInfoColumn contextID={props.contextID} />
                 <ContextToolColumn contextID={props.contextID} />
                 <RegenerationColumn contextID={props.contextID} />
