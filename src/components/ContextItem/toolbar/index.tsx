b98ca4df31abea4f62e3ca2d79e791c620bf1e60
Kevin Mok <mok.kevin11@gmail.com>
2022-12-20 12:23:01 -0600

    Add hover effects

--- src/components/ContextItem/toolbar/index.tsx
+++ src/components/ContextItem/toolbar/index.tsx
@@ -1,9 +1,47 @@
-import React, { ReactElement } from 'react';
+import React, { FC, ReactElement, useState } from 'react';
 import VisibilityOffRounded from '@mui/icons-material/VisibilityOffRounded';
+import VisibilityRounded from '@mui/icons-material/VisibilityRounded';
 
 type ToolProps = {
-    icon?: ReactElement;
+    icon?: FC<any>;
     label?: string;
+};
+
+const Tool = (props: ToolProps) => {
+    const [isHovered, setIsHovered] = useState(false);
+
+    return (
+        <div
+            className="flex items-center mr-1"
+            onMouseLeave={() => setIsHovered(false)}
+            onMouseEnter={() => setIsHovered(true)}
+        >
+            {props.icon && (
+                <props.icon
+                    {...{
+                        style: {
+                            color: isHovered
+                                ? 'var(--uxp-host-text-color-secondary)'
+                                : 'var(--uxp-host-text-color)',
+                        },
+                        fontSize: 'small',
+                    }}
+                />
+            )}
+            {props.label && (
+                <div
+                    className="ml-1"
+                    style={{
+                        color: isHovered
+                            ? 'var(--uxp-host-text-color-secondary)'
+                            : 'var(--uxp-host-label-text-color)',
+                    }}
+                >
+                    {props.label}
+                </div>
+            )}
+        </div>
+    );
 };
 
 const ToolbarDivider = () => {
@@ -12,26 +50,12 @@ const ToolbarDivider = () => {
     );
 };
 
-const Tool = (props: ToolProps) => {
-    const iconComponent = props?.icon;
-    return (
-        <div className="flex items-center">
-            {props.icon &&
-                React.cloneElement(iconComponent, {
-                    fontSize: 'small',
-                    className: 'hover:border',
-                })}
-            {props?.label}
-        </div>
-    );
-};
-
 const ContextToolbar = () => {
     return (
         <div className="flex w-full border-b border-[color:var(--uxp-host-border-color)] mb-1 p-1 items-center">
-            <div>ContextToolbar</div>
+            <Tool icon={VisibilityRounded} label="Unhide" />
+            <Tool icon={VisibilityOffRounded} label="Hide" />
             <ToolbarDivider />
-            <Tool icon={<VisibilityOffRounded />} label="Hide" />
         </div>
     );
 };
