7d32746ea407f445c3e7338b455875bfc4658816
Kevin Mok <mok.kevin11@gmail.com>
2022-12-19 20:44:48 -0600

    Add beginning toolbar component

--- src/components/ContextItem.tsx
+++ src/components/ContextItem.tsx
@@ -6,6 +6,7 @@ import React from 'react';
 import { ContextInfoColumn } from './ContextInfoColumn';
 import { ContextToolColumn } from './ContextToolColumn';
 import { RegenerationColumn } from './RegenerationColumn';
+import ContextToolbar from './ContextItem/toolbar';
 import { ContextLabel } from './ContextLabel';
 
 export type ContextItemProps = {
@@ -22,6 +23,7 @@ export const ContextItem = (props: ContextItemProps) => {
 
     return (
         <div className="flex flex-col p-1 bg-[color:var(--uxp-host-widget-hover-background-color)] border border-[color:var(--uxp-host-border-color)] rounded">
+            <ContextToolbar />
             <div className="flex">
                 <ContextInfoColumn contextID={props.contextID} />
                 <ContextToolColumn contextID={props.contextID} />
