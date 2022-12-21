7c28e131f7e2bdc83be14c99097c1ea546deffe7
Benjamin Garrard <b2gdevs@gmail.com>
2022-12-21 01:20:42 -0600

    added a remove context item button

--- src/components/RegenerationColumn.tsx
+++ src/components/RegenerationColumn.tsx
@@ -2,7 +2,7 @@ import { ProgressResponse } from 'common/types/sdapi';
 import LayerAIContext from 'models/LayerAIContext';
 import React, { useEffect } from 'react';
 import { useState, useRef } from 'react';
-import Spectrum, { Progressbar } from 'react-uxp-spectrum';
+import Spectrum, { Button, Progressbar } from 'react-uxp-spectrum';
 import {
     generateAILayer,
     getImageProcessingProgress,
@@ -45,6 +45,10 @@ export const RegenerationColumn = (props: RegenerationColumnProps) => {
     );
     let saveContextToStore = useContextStore(
         (state: ContextStoreState) => state.saveContextToStore
+    );
+
+    let removeContextFromStore = useContextStore(
+        (state: ContextStoreState) => state.removeContextFromStore
     );
 
     let contexts = useContextStore(
@@ -148,6 +152,9 @@ export const RegenerationColumn = (props: RegenerationColumnProps) => {
     return (
         <>
             <div className="flex flex-col justify-between">
+                <Button onClick={() => removeContextFromStore(props.contextID)}>
+                    Remove Generator Block(contextItem)
+                </Button>
                 <ProgressButton
                     disabled={false}
                     // We have to have a standard image size for bashing process.  We can't allocate that much Vram for high resolutions
