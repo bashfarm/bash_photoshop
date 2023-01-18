e49061043c03cfd3a1e010e010849cdd9b7006f0
Ben Garrard <B2Gdevs@users.noreply.github.com>
2023-01-18 12:16:03 -0600

    Bashful project loading (#17)
    * refactor components to be readable at a glance
    
    * working state using class store and method definitions on the class
    
    * added working loading state of project
    
    * refactor

--- src/services/bash_app_service.ts
+++ src/services/bash_app_service.ts
@@ -3,85 +3,102 @@ import { ContextStoreState } from 'store/contextStore';
 import { storage } from 'uxp';
 import {
     createTempFileEntry,
-    getTempFileEntry,
     saveActiveDocument,
+    saveTextFile,
 } from './io_service';
 import JSZip from 'jszip';
-import { BashfulAppProject } from '../constants';
+import { BashfulAppProject, DialogType } from '../constants';
 import photoshop from 'photoshop';
 import { executeInPhotoshop } from './middleware/photoshop_middleware';
-import { plainToInstance } from 'class-transformer';
 
 const lfs = storage.localFileSystem;
 
 export async function saveBashfulProject(contextStore: ContextStoreState) {
-    let copyOfState = _.cloneDeep(contextStore);
-    let tempFolder = await lfs.getTemporaryFolder();
-    let bashfulProjectEntry = await lfs.getFileForSaving('', {
-        types: ['bashful'],
-    });
-    if (bashfulProjectEntry.name.endsWith('.bashful') === false) {
-        bashfulProjectEntry.name = bashfulProjectEntry.name + '.bashful';
+    try {
+        let bashfulProjectEntry = await openBashfulFileDialog(DialogType.SAVE);
+
+        let stateFileEntry = await createStateDataEntry();
+        let psdFileEntry = await createPhotoshopFileEntry();
+
+        await saveTextFile(stateFileEntry, JSON.stringify(contextStore));
+        await saveActiveDocument(psdFileEntry);
+
+        let zip = new JSZip();
+        zip.file(stateFileEntry.nativePath, JSON.stringify(contextStore));
+        zip.file(
+            psdFileEntry.nativePath,
+            psdFileEntry.read({ format: storage.formats.binary })
+        );
+        let zipBlob = await zip.generateAsync({ type: 'arraybuffer' });
+        bashfulProjectEntry.write(zipBlob, { format: storage.formats.binary });
+    } catch (e) {
+        console.error(e);
     }
-
-    console.log(bashfulProjectEntry);
-
-    let stateFileEntry = (await tempFolder.createEntry(
-        BashfulAppProject.BASHFUL_STATE_FILE_NAME,
-        { type: storage.types.file, overwrite: true }
-    )) as storage.File;
-    let psdFileEntry = (await tempFolder.createEntry(
-        BashfulAppProject.BASHFUL_PHOTOSHOP_FILE_NAME,
-        { type: storage.types.file, overwrite: true }
-    )) as storage.File;
-    stateFileEntry.write(JSON.stringify(copyOfState), {
-        format: storage.formats.utf8,
-    });
-    await saveActiveDocument(psdFileEntry);
-    let zip = new JSZip();
-    zip.file(stateFileEntry.nativePath, JSON.stringify(copyOfState));
-    zip.file(
-        psdFileEntry.nativePath,
-        psdFileEntry.read({ format: storage.formats.binary })
-    );
-    let zipBlob = await zip.generateAsync({ type: 'arraybuffer' });
-    bashfulProjectEntry.write(zipBlob, { format: storage.formats.binary });
 }
 
-export async function loadBashfulProject(contextSetter: Function) {
-    let bashfulProjectEntry = (await lfs.getFileForOpening({
-        allowMultiple: false,
-    })) as storage.File;
-    console.log(contextSetter);
-    try {
-        let zip = await JSZip.loadAsync(
-            bashfulProjectEntry.read({ format: storage.formats.binary })
-        );
-        console.log(zip);
-        let photoshopFileEntry = await createTempFileEntry(
-            BashfulAppProject.BASHFUL_PHOTOSHOP_FILE_NAME
-        );
-        let stateEntry = await createTempFileEntry(
-            BashfulAppProject.BASHFUL_STATE_FILE_NAME
-        );
+async function createStateDataEntry() {
+    return await createTempFileEntry(BashfulAppProject.BASHFUL_STATE_FILE_NAME);
+}
 
-        let stateData = await zip.files[stateEntry.nativePath].async('string');
+async function createPhotoshopFileEntry() {
+    return await createTempFileEntry(
+        BashfulAppProject.BASHFUL_PHOTOSHOP_FILE_NAME
+    );
+}
+
+async function extractStateData(zip: JSZip) {
+    let stateEntry = await createStateDataEntry();
+    return await zip.files[stateEntry.nativePath].async('string');
+}
+
+async function extractPhotoshopFile(zip: JSZip) {
+    try {
+        let photoshopFileEntry = await createPhotoshopFileEntry();
+
         // Save the photoshop file that is in the zip file as a file that we can load.
         let data = await zip.files[photoshopFileEntry.nativePath].async(
             'arraybuffer'
         );
         photoshopFileEntry.write(data, { format: storage.formats.binary });
-        let newStateObj = plainToInstance(
-            ContextStoreState,
-            JSON.parse(stateData)
-        );
-        console.log(newStateObj);
-        contextSetter(newStateObj);
+        return photoshopFileEntry;
+    } catch (e) {
+        console.error(e);
+    }
+}
 
-        // Load the photoshop file
-        await executeInPhotoshop(loadBashfulProject, async () => {
-            await photoshop.app.open(photoshopFileEntry);
+async function openBashfulFileDialog(dialogType: DialogType) {
+    if (dialogType === DialogType.SAVE) {
+        let bashfulProjectEntry = await lfs.getFileForSaving('', {
+            types: ['bashful'],
         });
+
+        return bashfulProjectEntry;
+    }
+
+    return (await lfs.getFileForOpening({
+        allowMultiple: false,
+    })) as storage.File;
+}
+
+async function loadPhotoshopFile(photoshopFileEntry: storage.File) {
+    await executeInPhotoshop(loadPhotoshopFile, async () => {
+        await photoshop.app.open(photoshopFileEntry);
+    });
+}
+
+export async function getBashfulData() {
+    let bashfulProjectEntry = await openBashfulFileDialog(DialogType.OPEN);
+    return await JSZip.loadAsync(
+        bashfulProjectEntry.read({ format: storage.formats.binary })
+    );
+}
+
+export async function loadBashfulProject(contextSetter: Function) {
+    try {
+        let bashfulData = await getBashfulData();
+        let stateData = await extractStateData(bashfulData);
+        loadPhotoshopFile(await extractPhotoshopFile(bashfulData));
+        contextSetter(stateData);
     } catch (e) {
         console.error(e);
     }
